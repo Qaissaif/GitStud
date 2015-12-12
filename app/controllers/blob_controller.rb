5bb6255accdfe785c607fea91a89c605a5e8b12f
qaissaif <qais_saif@hotmail.com>
2015-12-12 19:35:04 +0200

    push

--- app/controllers/blob_controller.rb
+++ app/controllers/blob_controller.rb
@@ -1,5 +1,6 @@
 class BlobController < ApplicationController
 	before_filter :load_repository , :only=>[:show]
+      layout "code_layout"
 
 	def load_repository
 		if repo=Repository.where(:name=>params[:repository_id]).last
@@ -8,6 +9,8 @@ class BlobController < ApplicationController
 	end
 
 	def show
+	#render :layout => 'code_layout'
+
   	path=params[:id]
     @blob= Gitlab::Git::Blob.find(@repository,params[:ref],path)
     @html=CodeRay.scan(@blob.data, :ruby).div(:line_numbers => :table,:css=>:class)
